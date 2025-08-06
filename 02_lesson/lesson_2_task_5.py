981d8d6bb776da9defc1a376f616f9ce9fd45c31
ornela16 <ornela1616@gmail.com>
2025-08-07 01:33:55 +0300

    lesson2

--- 02_lesson/lesson_2_task_5.py
+++ 02_lesson/lesson_2_task_5.py
@@ -2,12 +2,12 @@
 def month_to_season(n):
 
     if n in (1,2,12):
-        return "сезон Зима"
+        print("сезон Зима")
     if n in (3,4,5):
-        return "сезон Весна"
+        print("сезон Весна")
     if n in (6,7,8):
-        return "сезон Лето"
+        print("сезон Лето")
     if n in (9,10,11):
-        return "сезон Осень"
+        print("сезон Осень")
 
 month_to_season(int(input("Номер месяца n = ")))
