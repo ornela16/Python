bb936602a2322857600cdb04007a0f9f13b593ff
ornela16 <ornela1616@gmail.com>
2025-08-06 22:47:35 +0300

    lesson2

--- 02_lesson/lesson_2_task_4.py
+++ 02_lesson/lesson_2_task_4.py
@@ -1,25 +1,28 @@
 
 def fizz_buzz(n):
 
-     for num in range(1, (n + 1)):
-        print(num)
+    for num in range(1, (n + 1)):
 
-        if num % 3 == 0 and num % 5 == 0:
+        if (num % 3 == 0 and num % 5 == 0):
             print('FizzBuzz')
-        elif num % 3 == 0:
-            print('Fizz')
-        elif num % 5 == 0:
-            print('Buzz')
+        else:
+
+            if (num % 3 == 0):
+                print('Fizz')
+            else:
+
+                if (num % 5 == 0):
+                    print('Buzz')
+                else:
+                    print(num)
 
 fizz_buzz(int(input("n = ")))
 
-# Это не работает как показано в примере, но как сделать не могу додуматься! Fizz Buzz FizzBuzz
-# печатается после числа, а не вместо! Подскажите, пожалуйста, как сделать.
-# Могу вывести # только простой список.у вывести только простой список
+# простой список
 
-def fizz_buzz(n):
-
-    for n in range(1, (n + 1)):
-        print(n)
-
-fizz_buzz(int(input("n = ")))
+# def fizz_buzz(n):
+#
+#     for n in range(1, (n + 1)):
+#         print(n)
+#
+# fizz_buzz(int(input("n = ")))
