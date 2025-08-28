6a15089e07fe336cd2f6cdc476aebbbdc2b97c73
ornela16 <ornela1616@gmail.com>
2025-08-28 13:02:37 +0300

    lesson3

---
+++ 04_lesson/test_string_utils.py
@@ -0,0 +1,70 @@
+from typing import AnyStr
+
+import pytest
+from string_utils import StringUtils
+
+string_utils = StringUtils()
+
+@pytest.mark.positive
+@pytest.mark.parametrize("input_str, expected", [
+    ("skypro", "Skypro"),
+    ("hello world", "Hello world"),
+    ("python", "Python") ] )
+def test_capitalize_positive(input_str, expected):
+    assert string_utils.capitalize(input_str) == expected
+
+@pytest.mark.negative
+@pytest.mark.parametrize("input_str, expected", [
+    ("123abc", "123abc"),
+    ("", ""), ("   ", "   "), ("-.","-.") ] )
+def test_capitalize_negative(input_str, expected):
+    assert string_utils.capitalize(input_str) == expected
+
+
+@pytest.mark.positive
+@pytest.mark.parametrize("input_str, expected", [ (" skypro", "skypro"),
+        (" python", "python") ] )
+def test_trim_positive(input_str, expected):
+    assert string_utils.trim(input_str) == expected
+
+@pytest.mark.negative
+@pytest.mark.parametrize("input_str, expected", [ ("  skypro", "skypro"),
+        (" 333", "333"), (" +7","+7"), (" ","") ] )
+def test_trim_negative(input_str, expected):
+    assert string_utils.trim(input_str) == expected
+
+
+@pytest.mark.positive
+@pytest.mark.parametrize("input_str, input_symbol, result", [ ("test","t",True),
+    ("Python","h",True), ("lesson","s",True), ("678","6",True) ] )
+def test_contains_positive(input_str, input_symbol, result):
+    res = string_utils.contains(input_str, input_symbol)
+    assert  res == result
+
+@pytest.mark.negative
+@pytest.mark.parametrize("input_str, input_symbol, result", [ ("test","b",False),
+    (" ","a",False), ("555","s",False), ("-symbol","",False), ("007","2",False)])
+def test_contains_negative(input_str, input_symbol, result):
+    res = string_utils.contains(input_str, input_symbol)
+    assert  res == result
+
+
+@pytest.mark.positive
+@pytest.mark.delete_symbol("input_str, input_symbol, result", [ ("Skyprof","f","Skypro"),("python","thon","py"),
+    ("01 сентября","0","1 сентября") ] )
+def test_delete_symbol_positive(input_str, input_symbol, result):
+    res = string_utils.delete_symbol(input_str, input_symbol)
+    assert res == result
+
+@pytest.mark.negative
+@pytest.mark.delete_symbol("input_str, input_symbol, result", [ ("Skypro","f","Skypro"),(" ","t"," "),
+    ("123","a","123") ] )
+def test_delete_symbol_negative(inp_str, inp_symbol, result):
+    res = string_utils.delete_symbol(inp_str, inp_symbol)
+    assert res == result
+
+@pytest.mark.negative
+@pytest.mark.delete_symbol("input_str, input_symbol, result", [ ("Skypro","None","Skypro") ] )
+def test_delete_symbol_negative(inp_str, inp_symbol, result):
+    res = string_utils.delete_symbol(inp_str, inp_symbol)
+    assert res == result
