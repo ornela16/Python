00e33550767c7def5bdd6f99fa9fa56aecd03e69
ornela16 <ornela1616@gmail.com>
2025-09-19 17:27:55 +0300

    lesson7

---
+++ 07_lesson/pages/calculator_page.py
@@ -0,0 +1,44 @@
+from unittest import result
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class CalculatorPage:
+  def __init__(self, driver):
+    self.driver = driver
+    self.driver.implicitly_wait(4)
+    self.driver.maximize_window()
+
+  def open(self):
+    self.driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+
+  def set_delay(self, value,):
+    self.driver.find_element(By.ID, "delay").clear()
+    self.driver.find_element(By.ID, "delay").send_keys(value)
+
+  def set_number7(self, value):
+    self.driver.find_element(By.XPATH, "//span[text()='7']").click()
+
+  def set_number8(self, value):
+    self.driver.find_element(By.XPATH, "//span[text()='8']").click()
+
+  def set_operator(self, value):
+    self. driver.find_element(By.XPATH, "//span[text()='+']").click()
+
+  def set_equal(self, value):
+    self.driver.find_element(By.CSS_SELECTOR, "span.btn.btn-outline-warning").click()
+
+  def result(self, value):
+
+    self.wait = WebDriverWait(self.driver, 50)
+    self.wait.until(
+      EC.text_to_be_present_in_element((By.CSS_SELECTOR, "div.screen"), "value"))
+
+    self.driver.find_element(By.CSS_SELECTOR, "div.screen")
+
+  def check(self):
+
+    result = self.driver.find_element(By.CSS_SELECTOR, "div.screen").text
+    assert result == "15"
---
+++ 07_lesson/test_07_calculator.py
@@ -0,0 +1,23 @@
+import pytest
+from selenium import webdriver
+from pages.calculator_page import CalculatorPage
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
+
+def test_calculator():
+    driver = webdriver.Chrome()
+    page = CalculatorPage(driver)
+    page.open()
+    page.set_delay(45)
+    page.set_number7("7")
+    page.set_operator("+")
+    page.set_number8("8")
+    page.set_equal("=")
+    page.result("15")
+    page.check()
+
