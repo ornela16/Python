27870d295d4cba83388dd6bc14efc6bd9eca1b86
ornela16 <ornela1616@gmail.com>
2025-09-30 12:46:53 +0300

    lesson8

---
+++ 08_lesson/test_08_create_project_YG_negativ.py
@@ -0,0 +1,35 @@
+
+import requests
+
+base_url = "https://ru.yougile.com/api-v2"
+API_key = ""
+headers = {"Authorization": "Bearer " + API_key, "Content-Type": "application/json"}
+
+# Негативный тест: не указано название проекта
+def test_create_project_negativ():
+    data = {"title": "", "users": {"f2d66cdf-21ed-4a73-9046-ec1bb366756b": "admin"}}
+    resp = requests.post(base_url + '/projects', json=data, headers=headers)
+    assert resp.status_code == 201
+    assert resp.json()["id"] is not None
+
+# Негативный тест: неверный метод создания проекта (get вместо post)
+def test_get_project():
+    data = {"title": "Newproject", "users": {"f2d66cdf-21ed-4a73-9046-ec1bb366756b": "admin"}}
+    resp = requests.get(base_url + '/projects', json=data, headers=headers)
+    id = resp.json()["id"]
+    resp2 = requests.get(base_url + '/projects/' + str(id), headers=headers)
+    assert resp2.status_code == 200
+    # Проверим название нового проекта:
+    assert resp2.json()["title"] == "Newproject"
+
+# Негативный тест: в запросе на изменение проекта непрописан headers)
+def test_edit_project():
+    data = {"title": "First_project", "users": {"f2d66cdf-21ed-4a73-9046-ec1bb366756b": "admin"}}
+    resp = requests.post(base_url + '/projects', json=data, headers=headers)
+    id = resp.json()["id"]
+    data = {"title": "Second_project"}
+    edited = requests.put(base_url + '/projects/' + str(id), json=data)
+    assert resp.json()["id"] is not None
+    assert edited.status_code == 200
+    resp2 = requests.get(base_url + '/projects/' + str(id), headers=headers)
+    assert resp2.status_code == 200
